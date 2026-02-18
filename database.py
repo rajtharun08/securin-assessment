b8f3b27122a9ad4b22dd7ba63690c7421f73e6b4
Tharun R <rajtharun08@gmail.com>
2026-02-18 11:22:16 +0530

    corrected database.py

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 import os
 from sqlalchemy.orm import sessionmaker , declarative_base
-from urllib.parse import quote_plus
+# from urllib.parse import quote_plus
 
-password = quote_plus("Tharun@08")
+# password = quote_plus("Tharun@08")
 
-url="mysql+pymysql://root:{password}@localhost:3306/recipe_db"
+url = "mysql+pymysql://root:Tharun%4008@localhost:3306/recipe_db"
 engine = create_engine(url)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
