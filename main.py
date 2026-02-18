a29e702cd7686bdcc6fdddf6ae443c127bf6ccaa
Tharun R <rajtharun08@gmail.com>
2026-02-18 12:19:36 +0530

    removed endpoint(search) and added endpoint recipes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,Depends,Query
 from sqlalchemy.orm import Session
+from sqlalchemy import desc
 from typing import List, Optional
 import tables;
 from database import engine, get_database
@@ -7,11 +8,14 @@ from database import engine, get_database
 app=FastAPI()
 tables.Base.metadata.create_all(bind=engine)
 
-@app.get("/recipes/search")
-def search_recipes(title: Optional[str] = None,cuisine: Optional[str] = None,db: Session = Depends(get_database)):
-    query = db.query(tables.Recipe)
-    if title:
-        query = query.filter(tables.Recipe.title.contains(title))
-    if cuisine:
-        query = query.filter(tables.Recipe.cuisine == cuisine)
-    return query.all()
+@app.get("/")
+def helloworld():
+    return {"hi":"helloworld"}
+
+@app.get("/recipes")
+def get_paginated_recipes(page: int = 1, limit: int = 10, db: Session = Depends(get_database)):
+    step=limit*(page-1)
+    
+    data = db.query(tables.Recipe).order_by(desc(tables.Recipe.rating)).offset(step).limit(limit).all()
+    return data
+
